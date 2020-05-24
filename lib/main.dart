81da919999304d4f11fb9aa69b924669d57c9b8d
Smriti Priya <smritikpriya@gmail.com>
2020-05-25 02:55:56 +0530

    a simple dice rolling app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(child: Text('Dicee')),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,9 +15,60 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+
+class _DicePageState extends State<DicePage> {
+  int lnum=1;
+  int rnum=1;
+  void change()
+  {
+    setState(() {
+      lnum=Random().nextInt(6)+1;
+      rnum=Random().nextInt(6)+1;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                onPressed:(){
+                  setState(() {
+                    change();
+                  });
+                },
+
+                child: Image.asset('images/dice$lnum.png')
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                onPressed: ()
+                {
+                  change();
+
+                },
+                child: Image.asset('images/dice$rnum.png'),
+
+              ),
+            )
+          ],
+        ),
+      ),
+
+    );
   }
 }
+
+
+
+
